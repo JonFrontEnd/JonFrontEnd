0c0a22466bf012d5a4357c15252a25ff6484f7f5
Jonias Silva <89999300+JonFrontEnd@users.noreply.github.com>
2021-11-22 16:37:33 -0300

    Projetos JavaScript

---
+++ controle/for.js
@@ -0,0 +1,21 @@
+let contador= 1
+while (contador <=8){
+    console.log(`Contador número  =  ${contador}`)
+    contador++
+
+}
+
+
+for ( let contador1 = 1 ; contador1 <=10 ; contador1++){
+    console.log(`Contador dois ${contador1}`)
+
+
+}
+
+
+const notas = [6.0, 7.0 , 8.4 , 9.7 , 6.1]
+for (let i = 0; i< notas.length; i++) {
+console.log(`Segue as notas ${notas [i]}`)
+}
+
+
